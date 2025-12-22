944008fa77b40816750a7bf5667a6252075282b9
mauryadarji04 <mauryadarji2006@gmail.com>
2025-12-22 16:24:32 +0530

    utils dont need i think not started yet

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,21 @@
+const asyncHandler = (reqHandler) => {
+    (req, res, next) => {
+        Promise.resolve(reqHandler(req, res, next)).
+        catch((err) => next(err))
+    }
+}
+
+export {asyncHandler}
+
+
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next);
+//     } catch (error) {
+//         res.status(500).json({ 
+//             sucess: false,
+//             message: error.message 
+//         });
+//     }
+// }
