3f04ae1d25f6c268071cb69c3efcb1c1b24234b3
mauryadarji04 <mauryadarji2006@gmail.com>
2025-12-29 15:38:30 +0530

    fix: update logoutUser to unset refreshToken and change update-account route method to PATCH

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -39,7 +39,7 @@ router.route("/change-password").post(verifyJWT, changeCurrentPassword)
 
 router.route("/current-user").get(verifyJWT, getCurrentUser)
 
-router.route("/update-account").put(verifyJWT, updateAccountDetails)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
 
 router.route("/update-avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
 
